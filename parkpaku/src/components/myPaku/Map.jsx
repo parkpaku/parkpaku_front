ddcc07b494495263387c81cf3880df122f0ff8f3
HyeongJun, Kim <khj029956@naver.com>
2024-11-14 20:25:51 +0900

    PR 전 merge

--- parkpaku/src/components/myPaku/Map.jsx
+++ parkpaku/src/components/myPaku/Map.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 import "./Map.css";
-import PakuModal from './PakuModal';
-import BottomSheet from './BottomSheet';
+import PakuModal from "./PakuModal";
+import BottomSheet from "./BottomSheet";
 
 function Map() {
   const mapRef = useRef(null);
   const { naver } = window;
-  
+
   useEffect(() => {
     if (!window.naver || !window.naver.maps) {
-        console.error("네이버 지도 API가 로드되지 않았습니다.");
-        return;
-      }
+      console.error("네이버 지도 API가 로드되지 않았습니다.");
+      return;
+    }
 
     // 네이버 지도 옵션 선택
     const mapOptions = {
@@ -25,34 +25,29 @@ function Map() {
       zoomControl: true, // 줌 컨트롤 표시
       zoomControlOptions: { position: 9 }, // 줌 컨트롤 우하단에 배치
     };
-    mapRef.current = new naver.maps.Map(
-      'map',
-      mapOptions
-    );
+    mapRef.current = new naver.maps.Map("map", mapOptions);
   }, []);
 
   const [modalOpen, setModalOpen] = useState(true);
   const items = [
-    { name: '삼락 생태공원', location: '부산 사상구', visits: 3 },
+    { name: "삼락 생태공원", location: "부산 사상구", visits: 3 },
     // 추가 아이템 데이터...
   ];
 
   return (
     <div>
-        <div>
-            <button>이전</button>
-            <h1>나의 Paku</h1> 
-        </div>
-        <div id="map">
-            
+      <div>
+        <button>이전</button>
+        <h1>나의 Paku</h1>
+      </div>
+      <div id="map">
         <h1>야</h1>
-        </div>
-        
-        <div>
-          {modalOpen && <PakuModal onClose={() => setModalOpen(false)} />}
-          <BottomSheet items={items} />
-        </div>
+      </div>
 
+      <div>
+        {modalOpen && <PakuModal onClose={() => setModalOpen(false)} />}
+        <BottomSheet items={items} />
+      </div>
     </div>
   );
 }
