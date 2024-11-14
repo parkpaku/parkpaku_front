92edcefb17987b4cd6a70b235159841b40a42c8c
HyeongJun, Kim <khj029956@naver.com>
2024-11-14 16:58:20 +0900

    Feat: 바텀바 수정

--- parkpaku/src/common/BottomBar.jsx
+++ parkpaku/src/common/BottomBar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
+import { MdForum } from "react-icons/md";
 import "./BottomBar.css";
 
 function BottomBar() {
   return (
     <nav className="bottom-bar">
-      <Link to="/">홈</Link>
-      <Link to="/other">기타</Link>
-      {/* 네비게이션 링크들 필요 시 추가 */}
+      <Link to="/" className="bottom-bar-item">
+        <AiFillHome size={24} />
+        <span>홈</span>
+      </Link>
+      <Link to="/community" className="bottom-bar-item">
+        <MdForum size={24} />
+        <span>커뮤니티</span>
+      </Link>
+      <Link to="/profile" className="bottom-bar-item">
+        <AiFillInfoCircle size={24} />
+        <span>내정보</span>
+      </Link>
     </nav>
   );
 }
